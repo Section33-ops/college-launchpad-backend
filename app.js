7025f2c2934f818f60cde4518629c3e607d85a8a
Saeed Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-08-18 20:54:02 -0400

    feat: install dotenv. also created api key as an env variable

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 const port = 8080;
+const apiKey = process.env.API_KEY;
 
 app.listen(port, () => {
   console.log(`college-launchpad-backend is running on port ${port}`);
