0a029f12e35681e7aef7e6f05095a4561c12441d
Saeed Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-08-18 21:42:13 -0400

    feat: add route to fetch list of colleges from the api

--- app.js
+++ app.js
@@ -3,7 +3,37 @@ import express from 'express';
 
 const app = express();
 const port = 8080;
-const apiKey = process.env.API_KEY;
+
+app.use(express.json());
+
+app.get('/colleges', async (req, res) => {
+  const baseUrl = process.env.COLLEGE_API_BASE_URL;
+  const apiKey = process.env.COLLEGE_API_KEY;
+  const colleges = [];
+  try {
+    const response = await fetch(`${baseUrl}?api_key=${apiKey}`);
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data = await response.json();
+
+    data.results.forEach((college) => {
+      colleges.push({
+        id: college.school.id,
+        name: college.school.name,
+        zipCode: college.school.zip,
+        city: college.school.city,
+        state: college.school.state,
+      });
+    });
+    res.json(colleges);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Could not fetch resource' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`college-launchpad-backend is running on port ${port}`);
