6194670726958ecdfd2a26356f5d532bfe61342a
Saeed Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-08-21 21:00:52 -0400

    feat: add function to be able to get state and city parameters from the get route

--- app.js
+++ app.js
@@ -12,8 +12,29 @@ app.get('/colleges', async (req, res) => {
   const baseUrl = process.env.COLLEGE_API_BASE_URL;
   const apiKey = process.env.COLLEGE_API_KEY;
   const colleges = [];
+
+  const { stateParam, cityParam } = req.query;
+
+  function getUrl() {
+    if (stateParam === undefined && cityParam === undefined) {
+      return `${baseUrl}?api_key=${apiKey}`;
+    }
+
+    if (stateParam && cityParam) {
+      return `${baseUrl}?api_key=${apiKey}&school.state=${stateParam}&school.city=${cityParam}`;
+    }
+
+    if (stateParam) {
+      return `${baseUrl}?api_key=${apiKey}&school.state=${stateParam}`;
+    }
+
+    if (cityParam) {
+      return `${baseUrl}?api_key=${apiKey}&school.city=${cityParam}`;
+    }
+  }
+
   try {
-    const response = await fetch(`${baseUrl}?api_key=${apiKey}`);
+    const response = await fetch(getUrl());
 
     if (!response.ok) {
       throw new Error('Could not fetch resource');
